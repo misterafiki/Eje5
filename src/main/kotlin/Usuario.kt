e4d34f6d89ad2edcbf9c795348c291a8403da1b6
diegogarciia <diegogarciaangel25@gmail.com>
2024-04-05 17:15:59 +0200

    Actualización Juego Adivinar Número

--- src/main/kotlin/Usuario.kt
+++ src/main/kotlin/Usuario.kt
@@ -67,27 +67,29 @@ class Usuario   /*(var email:String,
     fun adivinarNumero() {
 
         println("¡Bienvenido a la aplicación ADIVINA EL NÚMERO!")
+
         println("El juego consiste en adivinar un número secreto comprendido entre el número 1 y el número 20")
 
-        var cantidadPartidasJugadas = 0
+        var cantidadPartidasPerdidas = 0
         var cantidadPartidasGanadas = 0
         var continuarJugando = true
 
         while (continuarJugando) {
-
             val turnoMaximo = 10
             var turnoMinimo = 0
+            val numeroSecreto: Int = (1..20).random()
 
             while (turnoMinimo < turnoMaximo) {
                 turnoMinimo++
-                val numeroSecreto: Int = (1..20).random()
+
                 println("Dime tu número y te diré si es el número secreto")
                 val numeroSeleccionado: Int = readln().toInt()
 
-                if (numeroSeleccionado == numeroSecreto && turnoMinimo == turnoMaximo) {
+                if (numeroSeleccionado == numeroSecreto) {
                     println("¡ENHORABUENA! Has adivinado el número secreto, que era el número $numeroSecreto")
+
                     cantidadPartidasGanadas++
-                    turnoMinimo = 10
+                    turnoMinimo = turnoMaximo
 
                 } else {
                     println("Lo siento ese no es el número secreto")
@@ -95,20 +97,28 @@ class Usuario   /*(var email:String,
 
                 if (turnoMinimo == turnoMaximo && numeroSeleccionado != numeroSecreto) {
                     println("Lo siento, se han agotado los turnos, el número secreto es el número $numeroSecreto")
+
+                    cantidadPartidasPerdidas++
                 }
 
-                if (turnoMinimo == turnoMaximo) {
-                    cantidadPartidasJugadas++
-                }
             }
+
             println("¿Quieres seguir jugando? (s/n)")
+
             val respuesta = readLine()!!.toLowerCase()
+
             if (respuesta != "s") {
                 continuarJugando = false
             }
+
         }
+
+        val cantidadPartidasJugadas = cantidadPartidasGanadas + cantidadPartidasPerdidas
+
         println("Cantidad de partidas jugadas: $cantidadPartidasJugadas")
+
         println("Cantidad de partidas ganadas: $cantidadPartidasGanadas")
-        }
+
+    }
 
 }
