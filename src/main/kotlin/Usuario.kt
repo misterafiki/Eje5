9ce0b70a41251f12552fb4a9a3c91052b21f5e19
Rafael Becerra Guerra <rafikibece9@gmail.com>
2024-04-05 11:58:44 +0200

    version 0.0

--- src/main/kotlin/Usuario.kt
+++ src/main/kotlin/Usuario.kt
@@ -64,4 +64,51 @@ class Usuario   /*(var email:String,
         }
     }
 
+    fun adivinarNumero() {
+
+        println("¡Bienvenido a la aplicación ADIVINA EL NÚMERO!")
+        println("El juego consiste en adivinar un número secreto comprendido entre el número 1 y el número 20")
+
+        var cantidadPartidasJugadas = 0
+        var cantidadPartidasGanadas = 0
+        var continuarJugando = true
+
+        while (continuarJugando) {
+
+            val turnoMaximo = 10
+            var turnoMinimo = 0
+
+            while (turnoMinimo < turnoMaximo) {
+                turnoMinimo++
+                val numeroSecreto: Int = (1..20).random()
+                println("Dime tu número y te diré si es el número secreto")
+                val numeroSeleccionado: Int = readln().toInt()
+
+                if (numeroSeleccionado == numeroSecreto && turnoMinimo == turnoMaximo) {
+                    println("¡ENHORABUENA! Has adivinado el número secreto, que era el número $numeroSecreto")
+                    cantidadPartidasGanadas++
+                    turnoMinimo = 10
+
+                } else {
+                    println("Lo siento ese no es el número secreto")
+                }
+
+                if (turnoMinimo == turnoMaximo && numeroSeleccionado != numeroSecreto) {
+                    println("Lo siento, se han agotado los turnos, el número secreto es el número $numeroSecreto")
+                }
+
+                if (turnoMinimo == turnoMaximo) {
+                    cantidadPartidasJugadas++
+                }
+            }
+            println("¿Quieres seguir jugando? (s/n)")
+            val respuesta = readLine()!!.toLowerCase()
+            if (respuesta != "s") {
+                continuarJugando = false
+            }
+        }
+        println("Cantidad de partidas jugadas: $cantidadPartidasJugadas")
+        println("Cantidad de partidas ganadas: $cantidadPartidasGanadas")
+        }
+
 }
